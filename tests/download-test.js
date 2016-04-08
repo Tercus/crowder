21c7f360af65b3bd3f3582eb2efd3d598416e57c
Tercus <Terces86@gmail.com>
2016-04-08 15:50:03 +0200

    changed download test code
    save test-torrent paths and remove unneeded code parts

--- tests/download-test.js
+++ tests/download-test.js
@@ -2,20 +2,20 @@ var WebTorrent = require('webtorrent-hybrid')
 var client = new WebTorrent()
 var fs = require('fs')
 
-download = '1be670efcaa7113a086bdb547f451fc0f1d81da7'
+var sintel = 'https://webtorrent.io/torrents/sintel.torrent'
+var magnet = 'magnet:?xt=urn:btih:c3ac6be00f67c88118c71e4971d12c793b99b954&dn=LtUxcQ1.jpg&tr=udp%3A%2F%2Fexodus.desync.com%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=wss%3A%2F%2Ftracker.webtorrent.io'
+
+var download = magnet
 console.log('download: ' + download)
 client.add(download, { path: './download/' }, function (torrent) {
 	console.log('added torrent')
-	torrent.files.forEach(function (file) {
-		console.log('Started saving ' + file.name)
-		file.getBuffer(function (err, buffer) {
-			if (err) {
-				console.error('Error downloading ' + file.name)
-				return
-			}
-				fs.writeFile(file.name, buffer, function (err) {
-				console.error('Error saving ' + file.name)
-			})
-		})
+})
+client.on('torrent', function(torrent) {
+	console.log('Established connection')
+	torrent.on('done', function() {
+		console.log('Finished downloading file')
+	})
+	torrent.on('download', function(chunksize) {
+		console.log('progress: ' + torrent.progress)
 	})
 })
