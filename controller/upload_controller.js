634ac792412d7c12408ecd311b87975ff0e4d7d6
Tercus <Terces86@gmail.com>
2016-04-11 02:20:52 +0200

    Updated server-client communication
    Updated the way the data gets sent from the browser to the server. This
    helps me sending more information about the uploaded files than
    previously. Not only that, I could send config and user info as well.

--- controller/upload_controller.js
+++ controller/upload_controller.js
@@ -4,7 +4,6 @@ module.exports = {
 	load: function (request, reply) {
 		const template = require('../template.js')
 		const WebTorrent = require('webtorrent-hybrid')
-		const fs = require('fs')
 		var client = new WebTorrent()
 		const sqlite3 = require("sqlite3").verbose()
  		var file = 'test.db'
@@ -17,22 +16,30 @@ module.exports = {
 		} else {
 			//TODO: add check if torrent has already been added by another user
 			//		so, check for the infoHash in the database and return error
-			var download = request.payload
-			var opts = {
+      var formData = request.payload
+      var download = formData.infoHash
+      console.log('Extracted infoHash: ' + download)
+      
+      var opts = {
 				path: './storage/' + download + '/',
-				announce: ['http://localhost:8080', 'udp://localhost:8080', 'ws://localhost:8080']
+				announce: ['ws://localhost:8080']
 				}
-			client.add(download, opts, function (torrent) {
-				console.log('added torrent')
-				torrent.on('download', function (chunkSize) {
+			client.add(download, opts)
+      client.on('torrent', function(torrent){
+        console.log('Added torrent: ' + torrent.infoHash)
+        torrent.on('download', function (chunkSize) {
 					console.log('progress: ' + (torrent.progress * 100).toFixed(2) + '%')
 				})
-				torrent.on('done', function (torrent) {
-					console.log('Torrent downloaded')
-					db.all("INSERT INTO videos VALUES ('" + download + "', '','Test','test desc','')")
-					db.close()
-				})
-			})
+        torrent.on('done', savetodb)
+      })
 		}
+    
+    //Save the torrent to the database. I can do that early, because I already have all I know.
+    //TODO: Add an option do make the file public before upload is finished
+    function savetodb (torrent) {
+      console.log('Torrent downloaded')
+      db.all("INSERT INTO videos VALUES ('" + download + "', '','Test','test desc','')")
+      db.close()
     }
+  }
 }
