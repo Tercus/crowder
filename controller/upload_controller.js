84fb218d5128b2a009ee41a62aeb146b2c4c1998
Tercus <Terces86@gmail.com>
2016-04-11 03:01:03 +0200

    Saving to DB now includes title and description
    When a file gets uploaded, the Title and description now also gets
    stored in the database. WebRTC is still acting up from time to time.

--- controller/upload_controller.js
+++ controller/upload_controller.js
@@ -27,19 +27,19 @@ module.exports = {
 			client.add(download, opts)
       client.on('torrent', function(torrent){
         console.log('Added torrent: ' + torrent.infoHash)
+        savetodb(torrent)
         torrent.on('download', function (chunkSize) {
 					console.log('progress: ' + (torrent.progress * 100).toFixed(2) + '%')
 				})
-        torrent.on('done', savetodb)
+        //torrent.on('done', savetodb)
       })
+      //Save the torrent to the database. I can do that early, because I already have all I know.
+      //TODO: Add an option do make the file public before upload is finished
+      function savetodb (torrent) {
+        console.log('Torrent downloaded')
+        db.all("INSERT INTO videos VALUES ('" + download + "', 123,'" + formData.title + "','" + formData.desc + "','" + Date.now() + "')")
+        db.close()
+      }
 		}
-    
-    //Save the torrent to the database. I can do that early, because I already have all I know.
-    //TODO: Add an option do make the file public before upload is finished
-    function savetodb (torrent) {
-      console.log('Torrent downloaded')
-      db.all("INSERT INTO videos VALUES ('" + download + "', '','Test','test desc','')")
-      db.close()
-    }
   }
 }
