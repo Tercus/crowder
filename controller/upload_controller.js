0c9296954a506c7e9330ea88ae28ae94f2b99633
Tercus <Terces86@gmail.com>
2016-04-11 03:49:55 +0200

    Fixed data written to DB
    Fixed how the data is being written to the database.

--- controller/upload_controller.js
+++ controller/upload_controller.js
@@ -9,22 +9,22 @@ module.exports = {
  		var file = 'test.db'
  		var db = new sqlite3.Database(file)		
 		
-		console.log(request.method)
 		if(request.method === 'get') {
 			//just show the upload form
 			reply (template.filled('upload', {}))
 		} else {
 			//TODO: add check if torrent has already been added by another user
 			//		so, check for the infoHash in the database and return error
-      var formData = request.payload
-      var download = formData.infoHash
+      var sendData = request.payload
+      var download = sendData.infoHash
+      console.log(sendData)
       console.log('Extracted infoHash: ' + download)
       
       var opts = {
 				path: './storage/' + download + '/',
 				announce: ['ws://localhost:8080']
 				}
-			client.add(download, opts)
+			client.add(download, opts, (err) => { if (err) console.log('Error: ' + err) })
       client.on('torrent', function(torrent){
         console.log('Added torrent: ' + torrent.infoHash)
         savetodb(torrent)
@@ -37,7 +37,7 @@ module.exports = {
       //TODO: Add an option do make the file public before upload is finished
       function savetodb (torrent) {
         console.log('Torrent downloaded')
-        db.all("INSERT INTO videos VALUES ('" + download + "', 123,'" + formData.title + "','" + formData.desc + "','" + Date.now() + "')")
+        db.all("INSERT INTO videos VALUES ('" + download + "', 123,'" + sendData.title + "','" + sendData.desc + "','" + Date.now() + "')")
         db.close()
       }
 		}
