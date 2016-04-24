9b9933ed177514c7211c257ca535081dec4f0b18
Tercus <Terces86@gmail.com>
2016-04-24 22:22:36 +0200

    removed socket.io
    socket.io not needed.

--- server.js
+++ server.js
@@ -5,42 +5,6 @@ const routes = require('./routes/routes.js')
 
 //Initialize hapi-server and set port
 server.connection({ port: 80, labels: ['web'] })
-server.connection({ port: 81, labels: ['tracker'] });
-server.connection({ port: 82, labels: ['seed'] })
-
-//Add socket.io to server listener
-var trackerio = require('socket.io')(server.select('tracker').listener)
-var seedio = require('socket.io')(server.select('seed').listener)
-
-//events from the tracker
-trackerio.on('connection', function (socket) {
-  socket.emit('Oh hii!')
-  socket.on('burp', function () {
-    console.log('Did you just burp?!')
-    socket.emit('what','*blank stare*')
-  })
-  console.log('New tracker: ' + socket.id)
-  seedio.clients(function(error, clients){
-    if (error) throw error;
-    console.log('All tracker: ' + clients) 
-  })
-})
-
-//events from the seed
-seedio.on('connection', function (socket) {
-  socket.emit('Oh hii!')
-  socket.on('burp', function () {
-    console.log('Did you just burp?!')
-    socket.emit('what','blank stare')
-  })
-  console.log('New seed: ' + socket.id)
-  seedio.clients(function(error, clients){
-    if (error) throw error;
-    console.log('All seeds: ' + clients) 
-  })
-})
-
-
 
 //Initialize routes
 routes.init(server)
@@ -49,6 +13,4 @@ routes.init(server)
 server.start((err) => {
     if (err) throw err
     console.log('Webserver listening on: ' + server.select('web').info.uri)
-    console.log('Tracker events on: ' + server.select('tracker').info.uri)
-    console.log('Seed events on: ' + server.select('seed').info.uri)
 })
