02b1b914fb6a8fe10a7437117b6268fb644807eb
Tercus <Terces86@gmail.com>
2016-05-04 01:46:09 +0200

    cleanup
    - removed old test files
    - removed test template files

--- tests/seed-test.js
+++
@@ -1,8 +0,0 @@
-var WebTorrent = require('webtorrent-hybrid')
-var client = new WebTorrent()
-var file = './testfile/testfile.jpg'
-
-client.seed(file, function (torrent) {
-	console.log('Seeding: ' + file)
-	console.log(torrent.infoHash)
-})
